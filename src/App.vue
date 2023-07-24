39b56ea34a0fb7c58f6b45d4aaa76b47bf1695d1
salazarmelita <asalazar@hyl.cl>
2023-07-24 04:01:55 -0400

    feat: edit and delete configured, localStorage added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <script setup>
-import {ref, reactive} from 'vue'
+import { ref, reactive, watch, onMounted } from 'vue'
+import { uid } from 'uid'
 import Header from './components/Header.vue'
 import Formulario from './components/Formulario.vue'
+import Paciente from './components/Paciente.vue';
 
-const pacientes = ref([0])
+const pacientes = ref([])
 
 const paciente = reactive({
+  id: null,
+  nombre: '',
+  propietario: '',
+  email: '',
+  alta: '',
+  sintoma: '',
+})
+
+watch(pacientes, ()=> {
+  guardarLocalStorage()
+}, {
+  depp: true
+})
+
+const guardarLocalStorage = () => {
+  localStorage.setItem('paciente', JSON.stringify(pacientes.value))
+}
+
+onMounted(()=>{
+  const pacientesStorage = localStorage.getItem('pacientes')
+  if(pacientesStorage){
+    pacientes.value = JSON.parse(pacientesStorage)
+  }
+})
+
+const guardarPaciente = () => {
+  if(paciente.id) {
+    const { id } = paciente
+    const i = pacientes.value.findIndex(paciente => paciente.id === id)
+    pacientes.value[i] = {...paciente}
+  } else {
+    pacientes.value.push({
+    ...paciente, 
+    id: uid()
+  })
+  }
+
+  //  Reiniciar objeto - forma 1
+  // paciente.nombre = '',
+  // paciente.propietario = '',
+  // paciente.email = '',
+  // paciente.alta = '',
+  // paciente.sintoma = ''
+
+  //  Reiniciar objeto - forma 2
+  Object.assign(paciente, {
     nombre: '',
     propietario: '',
     email: '',
     alta: '',
-    sintoma:'' 
-})
+    sintoma: '',
+    id: null,
+  })
+
+}
+
+const actualizarPaciente = (id) => {
+  const pacienteEditar = pacientes.value.filter( paciente => paciente.id === id)[0]
+  Object.assign(paciente, pacienteEditar)
+}
+
+const eliminarPaciente = (id)=>{
+  pacientes.value = pacientes.value.filter(paciente => paciente.id !== id)
+}
 
 </script>
 
@@ -19,16 +79,16 @@ const paciente = reactive({
   <div class="container mx-auto mt-20">
     <Header />
     <div class="mt-12 md:flex">
-      <Formulario 
-        v-model:nombre="paciente.nombre"
-        v-model:propietario="paciente.propietario"
-        v-model:email="paciente.email"
-        v-model:alta="paciente.alta"
-        v-model:sintoma="paciente.sintoma"
-      />
+      <Formulario v-model:nombre="paciente.nombre" v-model:propietario="paciente.propietario"
+        v-model:email="paciente.email" v-model:alta="paciente.alta" @guardar-paciente="guardarPaciente" :id="paciente.id"/>
       <div class="md:w-1/2 md:h-screen overflow-y-scroll">
         <h3 class="font-black text-3xl text-center">Administra tus pacientes</h3>
-        <div v-if="pacientes.length > 0"></div>
+        <div v-if="pacientes.length > 0">
+          <p class="text-lg mt-5 text-center mb-10">
+            Información de <span class="text-indigo-600 font-bold">Pacientes</span>
+          </p>
+          <Paciente v-for="paciente in pacientes" :paciente="paciente" @actualizar-paciente="actualizarPaciente" @eliminar-paciente="eliminarPaciente"/>
+        </div>
         <div v-else class="mt-10 text-2xl">No hay pacientes</div>
       </div>
     </div>
